c36f89831bba0382db8f0beb738d53622f2f426b
unknown <iparshacov@gmail.com>
2021-02-24 14:52:18 +0500

    Сделал первый экран и модальное окно с адаптивом

---
+++ src/js/modal.js
@@ -0,0 +1,31 @@
+$(function(){
+	function showModal(id){
+		$(document.body).addClass('is-open-modal');
+		$(id).addClass('is-open');
+		console.log(id);
+	}
+	function hideModals(){
+		$(document.body).removeClass('is-open-modal');
+		$('.modal').removeClass('is-open');
+		
+	}
+
+	$(".header-button").on('click', function(e){
+		console.log(e);
+		showModal('#modal1');
+		
+ });
+ $(".modal-close").click( function(){
+    $(".modal").removeClass("is-open");
+  });
+	$(document).on('click', function(e){
+		if (!(
+		($(e.target).parents('.dialog').length)
+		||	($(e.target).hasClass('dialog'))
+		||	($(e.target).hasClass('header-button')))
+		) {
+			hideModals();
+		}
+	});
+
+});
